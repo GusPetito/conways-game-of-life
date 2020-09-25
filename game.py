2f467567fdb0c1a4ded194b8647124da8dc89410
GusPetito <guspetito@gmail.com>
2020-09-25 01:15:04 -0400

    working simulation

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame as pg
 import numpy as np
 from enum import Enum
 import itertools
+import time
 
 # make constants
 # size of each square in pixels
@@ -12,7 +13,7 @@ screen_dimensions = (width*box_size, height*box_size)
 screen = pg.display.set_mode(screen_dimensions)
 line_color = (100, 100, 100)
 # in milliseconds
-tick_time = 500
+tick_time = 250
 
 # the mode we're currently in, whether we're placing or watching it go
 Mode = Enum('Mode', 'place simulate')
@@ -49,19 +50,30 @@ def get_neighbors(x, y):
             neighbor_sum += 1
     return neighbor_sum
 
+
+# what to do every tick logic-wise, and returns the new boxes array
+def logic_tick():
+    neighbors = np.zeros((width, height))
+    for index, box in np.ndenumerate(boxes):
+        neighbor_count = get_neighbors(*index)
+        neighbors[index] = neighbor_count == 3 or (neighbor_count == 2 and boxes[index])
+    return neighbors.copy()
+
+
 # set up PyGame
 pg.init()
 
 # game loop
 needs_to_update = True
+timer_start = time.time()
 while True:
-    # update boxes
-    if curr_mode == Mode.simulate and needs_to_update:
-        neighbors = np.zeros((width, height))
-        for index, box in np.ndenumerate(boxes):
-            neighbor_count = get_neighbors(*index)
-            neighbors[index] = neighbor_count == 3 or (neighbor_count == 2 and boxes[index])
-        boxes = neighbors.copy()
+    # tick
+    if curr_mode == Mode.simulate:
+        time_surpassed = (time.time() - timer_start) * 1000
+        if time_surpassed >= tick_time:
+            boxes = logic_tick()
+            needs_to_update = True
+            timer_start = time.time()
 
     if needs_to_update:
         screen.fill(0)
@@ -90,6 +102,7 @@ while True:
             elif event.key == pg.K_s:
                 curr_mode = Mode.simulate
                 boxes = boxes_orig.copy()
+                timer_start = time.time()
                 needs_to_update = True
         # check if user clicked in box
         if curr_mode == Mode.place and event.type == pg.MOUSEBUTTONDOWN:
